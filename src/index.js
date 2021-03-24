0ffac3a840959934b7a6e102458f448256d91011
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-24 12:04:23 -0400

    show in menu

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    title: "Kane Series Hub"
+    title: "Kane Series Hub",
+    hasShadow: true
   });
 
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
@@ -30,7 +31,3 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
